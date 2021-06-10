c0211d888ca3f2450edbf156ec176afd795dc76c
vparantza <69887917+vparantza@users.noreply.github.com>
2021-06-10 14:56:23 -0500

    Add files via upload

---
+++ ViewController.cs
@@ -0,0 +1,58 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using CoreGraphics;
+using Foundation;
+using AVFoundation;
+
+namespace App4_Acceso
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            btnVideoLocal.TouchUpInside += delegate
+            {
+                AVPlayer Reproductor;
+                AVPlayerLayer CapadeReproductor;
+                AVAsset Recurso;
+                AVPlayerItem RecursoReproducir;
+
+                Recurso = AVAsset.FromUrl(NSUrl.FromFilename("warningsigns.mp4"));
+                RecursoReproducir = new AVPlayerItem(Recurso);
+                Reproductor = new AVPlayer(RecursoReproducir);
+                CapadeReproductor = AVPlayerLayer.FromPlayer(Reproductor);
+                CapadeReproductor.Frame = new CGRect(50, 100, 300, 300);
+                View.Layer.AddSublayer(CapadeReproductor);
+                Reproductor.Play();
+            };
+            btnVideoInternet.TouchUpInside += delegate
+            {
+                AVPlayer Reproductor;
+                AVPlayerLayer CapadeReproductor;
+                AVAsset Recurso;
+                AVPlayerItem RecursoReproducir;
+
+                Recurso = AVAsset.FromUrl(NSUrl.FromString("https://example-files.online-convert.com/video/mp4/example.mp4"));
+                RecursoReproducir = new AVPlayerItem(Recurso);
+                Reproductor = new AVPlayer(RecursoReproducir);
+                CapadeReproductor = AVPlayerLayer.FromPlayer(Reproductor);
+                CapadeReproductor.Frame = new CGRect(50, 340, 300, 300);
+                View.Layer.AddSublayer(CapadeReproductor);
+                Reproductor.Play();
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,27 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace App4_Acceso
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        UIKit.UIButton btnVideoInternet { get; set; }
+
+
+        [Outlet]
+        UIKit.UIButton btnVideoLocal { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
